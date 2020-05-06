4141030a6792a7ed871356313f6a590ace2d7935
saandhyarao <63493849+saandhyarao@users.noreply.github.com>
2020-05-06 17:02:31 +0530

    Create IntroductionTask3.js

---
+++ IntroductionTask3.js
@@ -0,0 +1,11 @@
+  <script>
+     /*
+	Write the program based on JavaScript objects and the following 
+	datatypes in JavaScript using JavaScript Editor
+	a.Boolean
+	b.Null
+	c.Undefined
+	d.Number
+    */
+
+</script>
