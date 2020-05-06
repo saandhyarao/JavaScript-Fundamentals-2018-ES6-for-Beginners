71793ae69337230d769592c7da261acfa3041bcd
saandhyarao <63493849+saandhyarao@users.noreply.github.com>
2020-05-06 17:13:58 +0530

    Create JavaScriptMapTask3.js

---
+++ JavaScriptMapTask3.js
@@ -0,0 +1,8 @@
+<script>
+	/*
+	1.Write the program to set the following objects in JavaScript using JavaScript Editor
+	a. String
+	b. Array
+	2.Write the program to fetch the data in JavaScript using JavaScript Editor
+	*/
+</script>
