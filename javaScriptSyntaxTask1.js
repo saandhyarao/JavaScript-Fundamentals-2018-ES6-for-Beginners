821e879daae030f3868fd0fd4952b6159f9b579f
saandhyarao <63493849+saandhyarao@users.noreply.github.com>
2020-05-06 17:26:33 +0530

    Create javaScriptSyntaxTask1.js

---
+++ javaScriptSyntaxTask1.js
@@ -0,0 +1,13 @@
+<script>
+     /*
+	Write the program using the following operators in JavaScript using JavaScript Editor
+	a.Arithmetic Operator
+	b.Comparison operator
+	c.Logical operator
+	d.Conditional Operator
+    */
+
+
+
+
+</script>
